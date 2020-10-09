979ac6d4f37a6e2101015bca1d6d1929a41ea0f6
Deutsch Gábor <deutschgabor92@gmail.com>
2020-10-10 01:39:58 +0200

    setups and done

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBZ9ENzkAWKlf6EiSfjipT1JA7S1_mpfvs",
+  authDomain: "password-generator-eac93.firebaseapp.com",
+  databaseURL: "https://password-generator-eac93.firebaseio.com",
+  projectId: "password-generator-eac93",
+  storageBucket: "password-generator-eac93.appspot.com",
+  messagingSenderId: "990659361538",
+  appId: "1:990659361538:web:8d83b3e2d3a59086a1e01b",
+  measurementId: "G-R2MEQ9S217",
+};
+
+const db = firebaseApp.firestore();
